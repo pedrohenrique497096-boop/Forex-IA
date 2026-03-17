204a0d3389cb0f4496f988a763ac6f70876f43f5
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-16 22:42:39 -0300

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import numpy as np
 
+def get_market_data(asset: str):
+
+    # Simulação inicial de candles
+    rows = 300
+
+    data = {
+        "open": np.random.uniform(100, 200, rows),
+        "high": np.random.uniform(200, 250, rows),
+        "low": np.random.uniform(90, 150, rows),
+        "close": np.random.uniform(100, 200, rows),
+        "volume": np.random.uniform(1000, 5000, rows),
+    }
+
+    df = pd.DataFrame(data)
+
+    return df
