74e9a4e6611bb0b1948b837389023995167c42d5
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-16 22:44:33 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,26 @@
+import json
+import os
+from datetime import datetime
 
+
+def save_history(result):
+
+    file = "history.json"
+
+    if os.path.exists(file):
+
+        with open(file, "r") as f:
+            try:
+                data = json.load(f)
+            except:
+                data = []
+
+    else:
+        data = []
+
+    result["timestamp"] = str(datetime.now())
+
+    data.append(result)
+
+    with open(file, "w") as f:
+        json.dump(data, f, indent=2)
