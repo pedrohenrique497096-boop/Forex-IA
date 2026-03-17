c0576383de2860cfc728c2b5e6c42a9c1529db57
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-16 22:45:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
+import streamlit as st
 
+from market_data import get_market_data
+from analysis_engine import analyze_asset
+from config.settings import ASSETS
+
+
+st.set_page_config(page_title="Forex AI", layout="wide")
+
+st.title("FOREX AI")
+st.write("Análise automática de mercado")
+
+if st.button("Analisar mercado"):
+
+    for asset in ASSETS:
+
+        data = get_market_data(asset)
+
+        result = analyze_asset(asset, data)
+
+        st.markdown("---")
+
+        st.subheader(asset)
+
+        st.write("Direção:", result["direction"])
+        st.write("Confiança:", result["confidence"], "%")
+
+        st.write("Motivos da análise:")
+
+        for r in result["summary"]:
+            st.write("-", r)
